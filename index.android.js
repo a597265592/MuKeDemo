cd745a315af997041724b88fb4b63aa8b803c365
chenjie <597265592@qq.com>
2018-07-13 16:26:12 +0800

    提交代码

--- index.android.js
+++ index.android.js
@@ -4,62 +4,63 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Image,
+    View
 } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 
 export default class MuKeDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <TabNavigator>
-          <TabNavigator.Item
-              selected={this.state.selectedTab === 'home'}
-              title="Home"
-              renderIcon={() => <Image source={...} />}
-              renderSelectedIcon={() => <Image source={...} />}
-              badgeText="1"
-              onPress={() => this.setState({ selectedTab: 'home' })}>
-              {homeView}
-          </TabNavigator.Item>
-          <TabNavigator.Item
-              selected={this.state.selectedTab === 'profile'}
-              title="Profile"
-              renderIcon={() => <Image source={...} />}
-              renderSelectedIcon={() => <Image source={...} />}
-              renderBadge={() => <CustomBadgeView />}
-              onPress={() => this.setState({ selectedTab: 'profile' })}>
-              {profileView}
-          </TabNavigator.Item>
-        </TabNavigator>
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state={
+            selectedTab:'home',
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'home'}
+                        title="Home"
+                        renderIcon={() => <Image source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() => <Image source={require('./res/images/ic')}/>}
+                        badgeText="1"
+                        onPress={() => this.setState({selectedTab: 'home'})}>
+                        <View style = {styles.page1}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'profile'}
+                        title="Profile"
+                        renderIcon={() => <Image source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() => <Image source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'profile'})}>
+                        <View style = {styles.page2}></View>
+                    </TabNavigator.Item>
+                </TabNavigator>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    page1:{
+        flex:1,
+        backgroundColor:'red',
+    },
+    page2:{
+        flex:1,
+        backgroundColor:'yellow',
+    }
 });
 
 AppRegistry.registerComponent('MuKeDemo', () => MuKeDemo);
